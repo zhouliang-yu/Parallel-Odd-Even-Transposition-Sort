bf57f0472d4a06fc18d76b3e515b61687853a361
zhouliang-yu <120040077@link.cuhk.edu.cn>
2022-10-04 00:07:48 -0400

    finished codes awaiting debug

--- codes/odd_even_parallel_sort.cpp
+++ codes/odd_even_parallel_sort.cpp
@@ -5,15 +5,77 @@
 #include <chrono>
 
 
+int merge_sort(int n, int* a){
+    int b[n];
+    domerge_sort(a, 0, n, b);
+    return 0;
+}
+
+int domerge_sort(int *a, int start, int end, int *b){
+    if ((end - start) <= 1) return 0;
+
+    int mid = (end+start)/2;
+    domerge_sort(a, start, mid, b);
+    domerge_sort(a, mid,   end, b);
+    merge(&(a[start]), mid-start, &(a[mid]), end-mid, &(b[start]));
+    for (int i=start; i<end; i++)
+        a[i] = b[i];
+
+    return 0;
+
+}
+
+
+void printstat(int rank, int iter, char *txt, int *la, int n) {
+    printf("[%d] %s iter %d: <", rank, txt, iter);
+    for (int j=0; j<n-1; j++)
+        printf("%6.3lf,",la[j]);
+    printf("%6.3lf>\n", la[n-1]);
+}
+
+void MPI_Pairwise_Exchange(int localn, int *locala, int sendrank, int recvrank,
+                           MPI_Comm comm) {
+
+    /*
+     * the sending rank just sends the data and waits for the results;
+     * the receiving rank receives it, sorts the combined data, and returns
+     * the correct half of the data.
+     */
+    int rank;
+    int remote[localn];
+    int all[2*localn];
+    const int mergetag = 1;
+    const int sortedtag = 2;
+
+    MPI_Comm_rank(comm, &rank);
+    if (rank == sendrank) {
+        MPI_Send(locala, localn, MPI_INT, recvrank, mergetag, MPI_COMM_WORLD);
+        MPI_Recv(locala, localn, MPI_INT, recvrank, sortedtag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    } else {
+        MPI_Recv(remote, localn, MPI_INT, sendrank, mergetag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        merge(locala, localn, remote, localn, all);
+
+        int theirstart = 0, mystart = localn;
+        if (sendrank > rank) {
+            theirstart = localn;
+            mystart = 0;
+        }
+        MPI_Send(&(all[theirstart]), localn, MPI_INT, sendrank, sortedtag, MPI_COMM_WORLD);
+        for (int i=mystart; i<mystart+localn; i++)
+            locala[i-mystart] = all[i];
+    }
+}
+
+
 int main (int argc, char **argv){
 
     MPI_Init(&argc, &argv); 
 
     int rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);  
-    
     int p;//number of processes
-    MPI_Comm_size(MPI_COMM_WORLD, &p);
+    comm = MPI_COMM_WORLD;
+    MPI_Comm_size(comm, &p); // get the number of processes
+    MPI_Comm_rank(comm, &rank);  //get the rank of the current processes
 
     int global_n; // number of elements to be sorted
     
@@ -48,8 +110,30 @@ int main (int argc, char **argv){
     */
 
     int local_n = global_n / p; // number of elements allocated to each process
-    int my_element[local_n]; // store elements of each process
-    MPI_Scatter(elements, local_n, MPI_INT, my_element, local_n, MPI_INT, 0, MPI_COMM_WORLD); // distribute elements to each process
+    int local_a[local_n]; // local array
+
+    MPI_Scatter(elements, local_n, MPI_INT, local_a, local_n, MPI_INT, 0, comm); // distribute elements to each process
+    merge_sort(local_n, local_a);
+    
+    int i;
+    //odd-even part
+    for (i = 1; i <= p; i++) {
+
+        printstat(rank, i, "before", local_a, local_n);
+
+        if ((i + rank) % 2 == 0) {  // means i and rank have same nature
+            if (rank < p - 1) {
+                MPI_Pairwise_Exchange(local_n, local_a, rank, rank + 1, comm);
+            }
+        } else if (p > 0) {
+            MPI_Pairwise_Exchange(local_n , local_a, rank - 1, rank, comm);
+        }
+
+    }
+
+    printstat(rank, i-1, "after", local_a, local_n);
+
+
     MPI_Gather(my_element, local_n, MPI_INT, sorted_elements, local_n, MPI_INT, 0, MPI_COMM_WORLD); // collect result from each process
     
     /* TODO END */
@@ -57,8 +141,8 @@ int main (int argc, char **argv){
     if (rank == 0){ // record time (only executed in master process)
         t2 = std::chrono::high_resolution_clock::now();  
         time_span = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
-        std::cout << "Student ID: " << "119010001" << std::endl; // replace it with your student id
-        std::cout << "Name: " << "Your Name" << std::endl; // replace it with your name
+        std::cout << "Student ID: " << "120040077" << std::endl; // replace it with your student id
+        std::cout << "Name: " << "Zhouliang_YU" << std::endl; // replace it with your name
         std::cout << "Assignment 1" << std::endl;
         std::cout << "Run Time: " << time_span.count() << " seconds" << std::endl;
         std::cout << "Input Size: " << global_n << std::endl;
